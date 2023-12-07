138c59cffa2d07aa05f719a4b25c6736e97898e6
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2023-12-07 17:28:40 +0000

    project setup + fixed bugs

---
+++ lib/post_repository.py
@@ -0,0 +1,22 @@
+from lib.post import Post
+
+
+class PostRepository:
+    def __init__(self):
+        self.store = []
+
+    def all(self):
+        return self.store
+
+    def all_by_tag(self, tag):
+        return [post for post in self.store if tag in post.tags]
+
+    def create(self, post):
+        post.id = self._generate_next_id()
+        self.store.append(post)
+        return post
+
+    def _generate_next_id(self):
+        if len(self.store) > 0:
+            return max([post.id for post in self.store]) + 1
+        return 1
